7f6747dbeacf23b0bbbae51a5469ebe22421807a
nurfianqodar <77nurfianqodar@gmail.com>
2025-07-26 13:19:42 +0700

    fix firebase variables

--- app/lib/firebase/index.ts
+++ app/lib/firebase/index.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // DOCS https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig: FirebaseOptions = {
   apiKey: import.meta.env.FB_API_KEY,
-  authDomain: import.meta.env.FB_API_KEY,
-  projectId: import.meta.env.FB_API_KEY,
-  storageBucket: import.meta.env.FB_API_KEY,
-  messagingSenderId: import.meta.env.FB_API_KEY,
-  appId: import.meta.env.FB_API_KEY,
-  measurementId: import.meta.env.FB_API_KEY,
+  authDomain: import.meta.env.FB_AUTH_DOMAIN,
+  projectId: import.meta.env.FB_PROJECT_ID,
+  storageBucket: import.meta.env.FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FB_APP_ID,
+  measurementId: import.meta.env.FB_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
